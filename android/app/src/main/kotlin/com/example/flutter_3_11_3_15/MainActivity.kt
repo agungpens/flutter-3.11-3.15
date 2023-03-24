88a64b1f470dd6c566ebd8943cb01374bc63778d
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-03-24 13:56:17 +0700

    flutter

---
+++ android/app/src/main/kotlin/com/example/flutter_3_11_3_15/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_3_11_3_15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("First Screen"),
+          ),
+          body: ElevatedButton(
+            onPressed: () {
+              // aksi ketika button diklik
+            },
+            child: Text("Tombol"),
+          ),
+        ));
+  }
+}
